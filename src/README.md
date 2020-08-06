92793497f47d2194d140088b0cf95d68e5232edc
lucassmedeiros <lucas28sampaio@gmail.com>
2020-08-06 13:37:55 -0300

    first commit

---
+++ src/README.md
@@ -0,0 +1,12 @@
+# Funcionalidade
+
+## Conexões
+
+- rota para listar o tatal de conexões realizadas
+
+- rota para criar uma nova conexao
+
+#Aulas
+
+-rota para criar aulas
+-rota para listar as aulas - filtrar por materia
---
+++ src/routes.ts
@@ -0,0 +1,16 @@
+import express from "express";
+
+import ClassesController from "./controllers/ClasssesControler";
+import ConnectionsController from "./controllers/ClasssesControler";
+
+const routes = express.Router();
+const classesControllers = new ClassesController();
+const connectionsController = new ConnectionsController();
+
+routes.get("/classes", classesControllers.index);
+routes.post("/classes", classesControllers.create);
+
+routes.post("/connections", connectionsController.create);
+routes.get("/connections", connectionsController.index);
+
+export default routes;
